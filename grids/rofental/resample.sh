3a2b31b732b6615f183bd2095c3d58847d62eb64
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-06-09 12:10:05 +0200

    Update resample.sh

--- grids/rofental/resample.sh
+++ grids/rofental/resample.sh
@@ -3,7 +3,7 @@
 src_res=50
 dst_res=1000
 
-for i in *_50.asc
+for i in *_$src_res.asc
 do
   gdal_translate -tr $dst_res $dst_res -r nearest -of AAIGrid $i `basename $i _$src_res.asc`_$dst_res.asc
 done
